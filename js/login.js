fe34dd52264bcae8769ba9b18f644a4aa991613e
Roxanne Franco <roxannefranco10@gmail.com>
2022-12-05 23:34:37 +0100

    Working log in with api and added access token in local storage

---
+++ js/login.js
@@ -0,0 +1,35 @@
+import { url } from "./config.js";
+
+const form = document.querySelector("#login-form");
+
+form.onsubmit = function (event) {
+  // prevents default submission
+  event.preventDefault();
+  const errorsLogin = document.querySelector("#errors-login");
+  errorsLogin.classList.add("hidden");
+  logInUser();
+};
+
+// check if inputs match registrated user data
+async function logInUser() {
+  const emailLogin = document.querySelector("#email-login");
+  const passwordLogin = document.querySelector("#password-login");
+  const data = {
+    email: emailLogin.value,
+    password: passwordLogin.value,
+  };
+  console.log(data);
+  let info = await fetch(`${url}/auth/login`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const response = await info.json();
+  console.log(response);
+  // set token to local storage
+  const accessToken = response.accessToken;
+  localStorage.setItem("accessToken", accessToken);
+  console.log(json);
+}
