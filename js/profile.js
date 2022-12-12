d6066e6277e3153dbb86a945edbf13011664809f
Roxanne Franco <roxannefranco10@gmail.com>
2022-12-12 22:10:45 +0100

    fetching each user posts on profile

--- js/profile.js
+++ js/profile.js
@@ -5,6 +5,8 @@ import {
   signOut,
   getToken,
   replaceUserProfile,
+  tagsGenerator,
+  reactionGenerator,
 } from "./functions.js";
 
 checkNotAuth();
@@ -39,13 +41,13 @@ getProfile();
 
 // getting user data
 async function userProfile(userInfo) {
-  const name = document.querySelector("#user-name");
+  const nameUser = document.querySelector("#user-name");
   const avatar = document.querySelector("#user-avatar");
   const followers = document.querySelector("#user-followers");
   const email = document.querySelector("#user-email");
   const avatarFollowers = document.querySelector("#avatar-followers");
 
-  name.innerHTML = userInfo.name;
+  nameUser.innerHTML = userInfo.name;
   if (userInfo.avatar != null && userInfo.avatar != "") {
     avatar.src = userInfo.avatar;
   }
@@ -66,3 +68,41 @@ async function userProfile(userInfo) {
 }
 
 // fetch posts by user name
+async function userPostsProfile() {
+  const response = await fetch(
+    `${url}/profiles/${name}/posts?_reactions=true`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+    }
+  );
+  const info = await response.json();
+
+  // adding user posts to user profile
+  const userPosts = document.querySelector("#user-posts");
+  info.forEach(function (post) {
+    // check if post has media to display
+    let media = "";
+    if (post.media !== "" && post.media != null) {
+      media = `<img class="mb-2 rounded-md" src="${post.media}">`;
+    }
+    userPosts.innerHTML += `
+    <a href="single.html?id=${
+      post.id
+    }" class="block mt-4 border border-neutral-300 hover:bg-neutral-100 rounded-md p-3">
+      <p class="text-neutral-800 mt-2 font-medium text-lg">${post.title}</p>
+      <p class="text-neutral-600 mb-2">${post.body}</p>
+      ${media}
+      <div class="flex items-center mb-2">
+        ${tagsGenerator(post.tags)}
+      </div>
+      <div class="flex items-center">
+        ${reactionGenerator(post.reactions)}
+      </div>
+    </a>`;
+  });
+}
+userPostsProfile();
